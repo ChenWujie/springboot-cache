8b5983d123e32d368ce206bf0519eaa3c9a8f64b
chenwujie <wujie.chen@qq.com>
2021-06-12 17:16:46 +0800

    redis访问测试

--- src/test/java/com/cwj/cache/SpringbootCacheApplicationTests.java
+++ src/test/java/com/cwj/cache/SpringbootCacheApplicationTests.java
@@ -5,12 +5,27 @@ import com.cwj.cache.mapper.EmployeeMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 
 @SpringBootTest
 class SpringbootCacheApplicationTests {
     @Autowired
     EmployeeMapper employeeMapper;
+
+    @Autowired
+    RedisTemplate redisTemplate;    //操作k-v都是对象的
+
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;    //操作k-v都是字符串的
+
+    @Test
+    void redisTest() {
+        String s = stringRedisTemplate.opsForValue().get("msg");
+        System.out.println(s);
+    }
+
     @Test
     void contextLoads() {
         Employee employee = employeeMapper.getEmpById(1);
